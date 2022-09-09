cac8ad6ddfa24d41082f06f5fe7e222d4687dab5
Ward McKinley <wmck126@gmail.com>
2022-09-09 19:04:16 -0400

    trying expo

--- client/App.js
+++ client/App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <image
+    <View>
+      <Image
         source ={{uri: "https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Donkey_%28Shrek%29.png/130px-Donkey_%28Shrek%29.png"}}
-        style={{width: 200, height: 200}}
+        style={{height: 400, width: 300, marginTop: 50}}
         />
-      <StatusBar style="auto" />
+      <Text style={{marginLeft: 50}}>Hello from donkeh</Text>
     </View>
   );
 }

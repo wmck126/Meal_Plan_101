e99058594118a3a48d4bc5a3db51ffd22f22552d
Ward McKinley <wmck126@gmail.com>
2022-09-11 23:14:17 -0400

    Signup works in backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_145145) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_031242) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_08_145145) do
     t.integer "weight_goal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "meals", "courses"
